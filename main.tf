77de010f52152df9c8fceb898a07382db20b7bb4
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-11-11 20:17:40 -0600

    user

--- main.tf
+++ main.tf
@@ -10,4 +10,15 @@ module "subnets" {
   name     = each.value["name"]
   subnets  = each.value["subnet_cidr"]
   vpc_id   = aws_vpc.main.id
+  ngw = try(each.value["ngw"],false)
+  igw = try(each.value["ngw"],false)
+  env = var.env
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
 }
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -3,3 +3,5 @@ variable "subnets" {}
 variable "vpc_id" {}
 variable "AZ" {}
 variable "env" {}
+variable "ngw" {}
+variable "igw" {}
